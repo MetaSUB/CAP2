63645ff927d6242f315291fa4801b1afc29a21fe
David Danko <dcdanko@gmail.com>
2019-10-06 12:21:35 -0400

    Add a bunch of (untested) modules from CAP1

--- cap2/pipeline/short_read/krakenuniq.py
+++ cap2/pipeline/short_read/krakenuniq.py
@@ -6,6 +6,7 @@ from os.path import join, dirname, basename
 from ..config import PipelineConfig
 from ..utils.conda import CondaPackage
 from ..databases.taxonomic_db import TaxonomicDB
+from ..preprocessing.map_to_human import RemoveHumanReads
 
 
 class KrakenUniq(luigi.Task):
@@ -25,13 +26,20 @@ class KrakenUniq(luigi.Task):
         self.config = PipelineConfig(self.config_filename)
         self.out_dir = self.config.out_dir
         self.db = TaxonomicDB()
+        self.reads = RemoveHumanReads(
+            sample_name=sample_name, pe1=pe1, pe2=pe2, config_filename=config_filename
+        )
 
     def requires(self):
-        return self.pkg, self.db
+        return self.pkg, self.db, self.reads
 
     def output(self):
-        report = luigi.LocalTarget(join(self.out_dir, f'{self.sample_name}.report.tsv'))
-        read_assignments = luigi.LocalTarget(join(self.out_dir, f'{self.sample_name}.read_assignments.tsv'))
+        report = luigi.LocalTarget(
+            join(self.out_dir, f'{self.sample_name}.report.tsv')
+        )
+        read_assignments = luigi.LocalTarget(
+            join(self.out_dir, f'{self.sample_name}.read_assignments.tsv')
+        )
         return {
             'report': report,
             'read_assignments': read_assignments,
@@ -49,8 +57,8 @@ class KrakenUniq(luigi.Task):
             '--paired '
             '--preload '
             f'--db {self.db.krakenuniq_db} '
-            f'{self.pe1} '
-            f'{self.pe2} '
+            f'{self.reads.output()["nonhuman_reads"][0].path} '
+            f'{self.reads.output()["nonhuman_reads"][1].path} '
             f'> {read_assignments}'
         )
         subprocess.call(cmd, shell=True)

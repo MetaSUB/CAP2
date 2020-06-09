6263318134b9a4b19b3e3bde5d069ceb5fb4365a
David Danko <dcdanko@gmail.com>
2020-06-09 00:21:04 -0400

    fix: find bug

--- cap2/pangea/api.py
+++ cap2/pangea/api.py
@@ -71,5 +71,6 @@ def get_task_list_for_sample(sample, stage, upload=True, config_path='', cores=1
         tasks = [
             clean_reads,
             read_stats,
+            kraken2,
         ]
     return tasks

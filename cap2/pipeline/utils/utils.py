c353b8ab6cf8ae9f14a03b0f40b272baaaf64ecd
David Danko <dcdanko@gmail.com>
2020-06-09 14:38:53 -0400

    fix: parse fastq

--- cap2/pipeline/utils/utils.py
+++ cap2/pipeline/utils/utils.py
@@ -1,13 +1,15 @@
 
+import gzip
 from Bio import SeqIO
 
 
 def estimate_read_length(fastq_filename):
     """Return an estimated average read length for the specified path."""
     n, total_len = 0, 0
-    for i, rec in enumerate(SeqIO.parse(fastq_filename, 'fastq')):
-        if i > 1000:
-            break
-        n += 1
-        total_len += len(rec.seq)
+    with gzip.open(fastq_filename, "rt") as handle:
+        for i, rec in enumerate(SeqIO.parse(handle, 'fastq')):
+            if i > 1000:
+                break
+            n += 1
+            total_len += len(rec.seq)
     return total_len / n

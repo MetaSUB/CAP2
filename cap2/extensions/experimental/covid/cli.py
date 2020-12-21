a7c3d2acfa098201ee2db906e6cbd0edcfbf5c15
David Danko <dcdanko@gmail.com>
2020-12-20 22:51:43 -0500

    feat: run covid pipeline off of tag

--- cap2/extensions/experimental/covid/cli.py
+++ cap2/extensions/experimental/covid/cli.py
@@ -65,6 +65,77 @@ def get_task_list_for_sample(sample, config, threads):
     return tasks
 
 
+def _process_one_sample_chunk(chunk,
+                              scheduler_url, stage, upload, download_only,
+                              config, threads, clean_reads, workers):
+    tasks = []
+    for sample in chunk:
+        tasks += get_task_list_for_sample(sample, config, threads)
+    if not scheduler_url:
+        luigi.build(tasks, local_scheduler=True, workers=workers)
+    else:
+        luigi.build(tasks, scheduler_url=scheduler_url, workers=workers)
+    return chunk
+
+
+def _process_samples_in_chunks(samples,
+                               scheduler_url, stage, upload, download_only,
+                               config, threads, clean_reads, workers,
+                               batch_size, timelimit):
+    start_time, completed = time.time(), []
+    click.echo(f'Processing {len(samples)} samples', err=True)
+    for i, chunk in enumerate(chunks(samples, batch_size))
+        logging.basicConfig(
+            level=log_level,
+            format=f'(batch {i + 1}) ' + '%(levelname)s:%(message)s',
+        )
+        click.echo(f'Completed processing {len(completed)} samples', err=True)
+        if timelimit and (time.time() - start_time) > (60 * 60 * timelimit):
+            click.echo(f'Timelimit reached. Stopping.', err=True)
+            return completed
+        completed += _process_one_sample_chunk(
+            chunk,
+            scheduler_url, stage, upload, download_only,
+            config, threads, clean_reads, workers
+        )
+    return completed
+
+
+@run.command('tag')
+@click.option('-c', '--config', type=click.Path(), default='', envvar='CAP2_CONFIG')
+@click.option('--clean-reads/--all-reads', default=False)
+@click.option('--upload/--no-upload', default=True)
+@click.option('--download-only/--run', default=False)
+@click.option('--scheduler-url', default=None, envvar='CAP2_LUIGI_SCHEDULER_URL')
+@click.option('--max-attempts', default=2)
+@click.option('-b', '--batch-size', default=10, help='Number of samples to process in parallel')
+@click.option('-w', '--workers', default=1)
+@click.option('-t', '--threads', default=1)
+@click.option('--timelimit', default=0, help='Stop adding jobs after N hours')
+@click.option('--endpoint', default='https://pangea.gimmebio.com')
+@click.option('-e', '--email', envvar='PANGEA_USER')
+@click.option('-p', '--password', envvar='PANGEA_PASS')
+@click.option('--random-seed', type=int, default=None)
+@click.option('--tag-name', default='MetaSUB COVID-19 CAP')
+def cli_run_samples_from_tag(config, clean_reads, upload, download_only, scheduler_url,
+                             max_attempts,
+                             batch_size, workers, threads, timelimit,
+                             endpoint, email, password, random_seed,
+                             tag_name):
+    set_config(endpoint, email, password, None, None, name_is_uuid=True)
+    tag = PangeaTag(tag_name, email, password, endpoint)
+    samples = [
+        samp for samp in tag.pangea_samples(randomize=True, seed=random_seed)
+        if not clean_reads or samp.has_clean_reads()
+    ]
+    completed = _process_samples_in_chunks(
+        samples,
+        scheduler_url, stage, upload, download_only,
+        config, threads, clean_reads, workers,
+        batch_size, timelimit
+    )
+
+
 @run_cli.command('samples')
 @click.option('-c', '--config', type=click.Path(), default='', envvar='CAP2_CONFIG')
 @click.option('-l', '--log-level', default=30)
@@ -93,29 +164,13 @@ def cli_run_samples(config, log_level, clean_reads, upload, download_only, sched
         format='%(levelname)s:%(message)s',
     )
     set_config(endpoint, email, password, org_name, grp_name)
-    group = PangeaGroup(grp_name, email, password, endpoint, org_name)
-    start_time, completed = time.time(), []
     samples = [
         samp for samp in group.pangea_samples(randomize=True, seed=random_seed)
         if not clean_reads or samp.has_clean_reads()
     ]
-    click.echo(f'Processing {len(samples)} samples', err=True)
-    for i, chunk in enumerate(chunks(samples, batch_size)):
-        logging.basicConfig(
-            level=log_level,
-            format=f'(batch {i + 1}) ' + '%(levelname)s:%(message)s',
-        )
-        click.echo(f'Completed processing {len(completed)} samples', err=True)
-        if timelimit and (time.time() - start_time) > (60 * 60 * timelimit):
-            click.echo(f'Timelimit reached. Stopping.', err=True)
-            break
-        tasks = []
-        for sample in chunk:
-            tasks += get_task_list_for_sample(sample, config, threads)
-        if not scheduler_url:
-            luigi.build(tasks, local_scheduler=True, workers=workers)
-        else:
-            luigi.build(
-                tasks, scheduler_url=scheduler_url, workers=workers
-            )
-        completed += chunk
+    completed = _process_samples_in_chunks(
+        samples,
+        scheduler_url, stage, upload, download_only,
+        config, threads, clean_reads, workers,
+        batch_size, timelimit
+    )

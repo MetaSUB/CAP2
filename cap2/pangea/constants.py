8c5d399a1b916b53cab913eda0c4955b95fdaceb
David Danko <dcdanko@gmail.com>
2021-04-25 19:20:41 -0400

    chore: add modules to qc

--- cap2/pangea/constants.py
+++ cap2/pangea/constants.py
@@ -2,5 +2,5 @@
 CAP_WORK_ORDER_PROTO_NAME = 'cap'
 
 WORK_ORDER_PROTOS = {
-	'metasub_cap_qc': ('qc', '435ffcd4-a582-47d8-97b6-bf4f3a42aec5'),  # TODO no harcoded UUIDs
+	'metasub_cap_qc': ('fast', '435ffcd4-a582-47d8-97b6-bf4f3a42aec5'),  # TODO no harcoded UUIDs
 }

3e2463b9c85b4a77871325b8041fc0cc36c0d41c
David Danko <dcdanko@gmail.com>
2021-04-25 21:22:49 -0400

    chore: add more pipeline stages to work orders

--- cap2/pangea/constants.py
+++ cap2/pangea/constants.py
@@ -3,4 +3,6 @@ CAP_WORK_ORDER_PROTO_NAME = 'cap'
 
 WORK_ORDER_PROTOS = {
 	'metasub_cap_qc': ('fast', '435ffcd4-a582-47d8-97b6-bf4f3a42aec5'),  # TODO no harcoded UUIDs
+    'pre': ('pre', '62a7b78f-cb95-42d6-b956-68b45abe47f5'),
+    'reads': ('reads', 'f6949311-f60b-44b9-8fe4-22df2060a379'),
 }
